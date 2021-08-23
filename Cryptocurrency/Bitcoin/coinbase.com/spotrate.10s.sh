b95e308f2cc7ea736600e144c30150f704c15a4a
Brad Weslake <brad.weslake@gmail.com>
2021-08-23 12:21:05 +0800

    Update spotrate.10s.sh
    URI for Coinbase API has changed.

--- Cryptocurrency/Bitcoin/coinbase.com/spotrate.10s.sh
+++ Cryptocurrency/Bitcoin/coinbase.com/spotrate.10s.sh
@@ -5,4 +5,4 @@
 # <xbar.author.github>matryer</xbar.author.github>
 # <xbar.desc>Shows latest spot rate values (in USD) for Bitcoins in the Coinbase exchange.</xbar.desc>
 
-echo -n "BTC: $"; curl -s "https://coinbase.com/api/v1/prices/spot_rate?currency=USD" | egrep -o '"amount":"[0-9]+(\.)?([0-9]{0,2}")?' | sed 's/"amount"://'  | sed 's:^.\(.*\).$:\1:'
+echo -n "BTC: $"; curl -s "https://api.coinbase.com/v2/prices/BTC-USD/spot?currency=USD" | egrep -o '"amount":"[0-9]+(\.)?([0-9]{0,2}")?' | sed 's/"amount"://'  | sed 's:^.\(.*\).$:\1:'
